fcd2f8d1ced30044a8eafe1a1ab1715cde9aea7f
ljws93 <ljws93@naver.com>
2021-01-04 23:22:26 +0900

    1st end

---
+++ CustomerCredit.py
@@ -0,0 +1,127 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+### 신용잔고 확인 (7일분) - 오르면 안좋음
+
+def customerCredit():
+    try:
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        options.add_argument("disable-gpu")
+
+        driver = webdriver.Chrome("C:/selenium/chromedriver", options=options)
+        url = "https://finance.naver.com/sise/sise_deposit.nhn"  # 네이버_증시자금동향
+        driver.get(url)
+
+        xpath1 = "//*[@id='contentarea_left']/div[3]/table[1]/tbody/tr["
+        xpath2 = "]/td[4]"
+
+        numberDataList = [] # 숫자 데이터 넣음
+        perDataList = []    # (현제 데이터 - 이전 데이터) / 이전 데이터 의 % 값을 넣음
+        total = []  # 최종 결과값에 대한 리스트
+
+        plus_shock = 0  # 변동율이 +2.5%를 넘어 갈 경우 카운트됨
+        minus_shock = 0 # 변동율이 -2.5%를 넘어 갈 경우 카운트됨
+
+
+        # 신용잔고 수치 뽑아내기
+        for i in range(4,14):
+            if 4 <= i <= 8 or 12 <= i <= 13:
+                xpath = xpath1 + str(i) + xpath2
+                singleData = WebDriverWait(driver, 12).until(EC.presence_of_element_located((By.XPATH, xpath))).text
+
+                # 계산을 위해서 ,(콤마) 제거
+                singleData = singleData.replace(",", "")
+                numberDataList.append(singleData) # 최근 데이터의 인덱스가 빠름(예: 인덱스 0번에는 현재 or 오늘 데이터 들어감)
+
+        # 위에서 뽑아낸 데이터 계산하여 변동량으로 나타내기
+        for i in range(0, 6):
+            singleData = (int(numberDataList[i]) - int(numberDataList[i+1])) / int(numberDataList[i+1]) * 100
+            singleData = round(singleData, 2)   # 소수점 2자리 까지 반올림
+
+            # 변동율이 +-2.5%를 넘어 갈 경우 카운트
+            if singleData >= 2.5:
+                plus_shock += 1
+            elif singleData <= -2.5:
+                minus_shock -= 1
+
+            # 연산 결과가 +일 경우에는 +가 붙지 않기 때문에 붙여준다
+            # 뒤에 연속일 계산에서는 String일 필요가 있기 때문에 str로 변환한다
+            if singleData >= 0:
+                singleData = "+" + str(singleData)
+            else:
+                singleData = str(singleData)
+
+            perDataList.append(singleData)
+
+        # 변동량 데이터를 xx일 연속 감소 같은 문자열 데이터로 나타내기
+        stoper = 0
+        plus = 0
+        minus = 0
+        plus_plus = 0
+        minus_minus = 0
+
+        for i in range(0, 6):
+            singleData = perDataList[i]
+
+            # 첫 수급의 플러스, 마이너스 확인하기
+            if i == 0 and singleData[0] == "-":
+                minus = 1
+            elif i == 0 and singleData[0] == "+":
+                plus = 1
+
+
+            # 연속 + or - 확인하기
+            if i == 0 and plus == 1:
+                plus_plus = 1
+            elif i == 0 and minus == 1:
+                minus_minus = 1
+
+            # 부호의 경우 마이너스는 -가 붙지만 플러스는 바로 숫자로 표기됨
+            if i >= 1 and singleData[0] == "+" and plus_plus >= 1 and stoper == 0:
+                plus_plus += 1
+            elif i >= 1 and singleData[0] == "-" and plus_plus >= 1 and stoper == 0:
+                stoper = 1
+            elif i >= 1 and singleData[0] == "-" and minus_minus >= 1 and stoper == 0:
+                minus_minus += 1
+            elif i >= 1 and singleData[0] == "+" and minus_minus >= 1 and stoper == 0:
+                stoper = 1
+
+        # 다른 요소와 연계하기 위해 반환값 주기
+        if plus_shock >= 1 and minus_shock >= 1:
+            total.append(0)
+        elif plus_shock >= 1:
+            total.append(plus_shock)
+        elif minus_minus >= 1:
+            total.append(minus_minus)
+        else:
+            total.append(0)
+
+        if plus_plus >= 1:
+            total.append("+" + str(plus_plus))
+
+        elif minus_minus >= 1:
+            total.append("-" + str(minus_minus))
+
+        print("<신용잔고(6일간)>[+-2.5% 변동율, 연속 증가/감소]", end="")
+        print(total, end="")
+        print(driver.current_url)
+
+        return total
+    except:
+        print("# 신용잔고 데이터 가져오기 통신에러")
+        driver.quit()
+
+
+
+
+
+
+
+
+
+
+
---
+++ KospiTopTen.py
@@ -0,0 +1,210 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import datetime
+
+## 코스피 top10 종목에서 각 종목의 외국인 매매금액과 코스피 전체의 외국인 매매금액의 차이를 구하고
+## 조건에 따른 메시지를 출력하게 한다(7일간)
+
+def kospiTopTen():
+
+    ## 코스피 Top10 종목코드
+    # 혹시 top10에 순위 변경이 있으면 이 부분만 바꿔주면된다
+    # 코스피의 경우 "삼성전자우"는 뺏음 => 그래서 결국 top9임 현재
+    # 아래 종목 및 코드를 수정할 때는 종목명을 검색하여 관련 코드도 다 고친다
+    # 예: sumsungElec를 고치고 싶다 -> "삼성전자" 검색 후 메시지 전부 고치기
+    samsungElec = "005930"
+    skHigh = "000660"
+    lgChemi = "051910"
+    samsungBio = "207940"
+    celltrion = "068270"
+    naver = "035420"
+    samsungSDI = "006400"
+    hyundai = "005380"
+    kakao = "035720"
+
+    stockList = []  # 위 종목 코드를 담는 리스트
+
+    stockList.append(samsungElec)
+    stockList.append(skHigh)
+    stockList.append(lgChemi)
+    stockList.append(samsungBio)
+    stockList.append(celltrion)
+    stockList.append(naver)
+    stockList.append(samsungSDI)
+    stockList.append(hyundai)
+    stockList.append(kakao)
+
+
+    ## 종목별 반복(위 코드 변수 갯수만큼 반복)
+    for i in range(0, len(stockList)):
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        options.add_argument("disable-gpu")
+
+        driver = webdriver.Chrome("C:/selenium/chromedriver", options=options)
+        url1 = "https://finance.daum.net/quotes/A"
+        url2 = stockList[i]
+        url3 = "#influential_investors/home"
+        url = url1 + url2 + url3    # 다음금융_외인/기관_탭
+
+        driver.get(url)
+
+        foreignBuyingAmountList = []    # 외인 순매수량 리스트(7일간)
+        stockPriceList = [] # 해당 일 주식 종가 리스트(7일간)
+        priceXamountList = []   # 종가 * 순매수/매도량 값의 리스트(7일간)
+
+
+        ## 날짜별 반복
+        # 해당 주식의 외인 순매수량, 해당 날의 종가 정보 가져오기(7일간 기록)
+        for j in range(1, 8):
+
+            ## 먼저 해당 주식의 순매수량 가져오기
+            xpath1 = "//*[@id='boxContents']/div[4]/div[1]/div[3]/div[2]/div/table/tbody/tr["
+            xpath2 = "]/td[4]"
+
+            xpath = xpath1 + str(j) + xpath2
+
+            foreignBuyingAmount = WebDriverWait(driver, 12).until(EC.presence_of_element_located((By.XPATH, xpath))).text
+
+            # 제일 앞에 항상 +, -가 따라오기 때문에 이 +, - 부호를 제거
+            foreignBuyingAmount = foreignBuyingAmount[1:-1]
+
+            # 계산을 위해 각 데이터에 있는 ,(콤마) 제거
+            foreignBuyingAmount = foreignBuyingAmount.replace(",", "")
+
+            # 계산을 위해 int형으로 변경후 리스트에 추가하기
+            foreignBuyingAmountList.append(int(foreignBuyingAmount))
+
+
+            ## 해당 주식의 해당 날의 종가 가져오기
+            xpath2 = "]/td[6]"
+            xpath = xpath1 + str(j) + xpath2
+
+            stockPrice = WebDriverWait(driver, 12).until(EC.presence_of_element_located((By.XPATH, xpath))).text
+
+            # 계산을 위해 각 데이터에 있는 ,(콤마) 제거
+            stockPrice = stockPrice.replace(",", "")
+
+            # 계산을 위해 int형으로 변경후 리스트에 추가하기
+            stockPriceList.append(int(stockPrice))
+
+            ## 해당 주식의 순매도(매수)량 * 종가 (단위는 만 이므로 // 10000을 해준다)
+            # 즉, 각 종목에 대한 외인들의 매매 금액이다
+            priceXamountList.append((int(foreignBuyingAmount) * int(stockPrice)) // 100000000)
+
+        # print("priceXamountList: ", end= "")    # (삭제) 확인용
+        # print(priceXamountList)     # (삭제) 확인용
+
+
+        ## 7일간의 코스피 외인 순매수/매도 데이터를 가져온다
+        #(수정하기) - 코스피 일자별 매매 데이터 시작일과 각 종목의 외국인 매매 데이터 시작일은 다를 수 있음 어케함?
+        dateValue_start = 1   # 위의 문제점을 해결하기 위해 넣은 변수
+        dateValue_end = 8   # 위의 문제점을 해결하기 위해 넣은 변수
+        # 시간을 확인하여 19시 이전에 프로그램을 시작했으면 start = 2 / end = 9
+        # 19시 이후에 프로그램 시작했으면 dataValue = 1 / end = 8
+
+        # 9~19시 사이에 실시했다면 start = 2 / end = 9
+        if 9 <= datetime.datetime.now().hour <= 19:
+            dateValue_start = 2
+            dateValue_end = 9
+
+        # 주말은 항상 18시 이후로 생각한다
+        if datetime.datetime.now().strftime("%a") == "Sat" or datetime.datetime.now().strftime("%a") == "Sun":
+            dateValue_start = 1
+            dateValue_end = 8
+
+        kospiForeignBuying_List = []  # 코스피 외인 순매수/매도 데이터 리스트
+
+        ## 코스피에서 외인들의 매매금액을 가져온다(7일간)
+        for k in range(dateValue_start, dateValue_end):
+            options = webdriver.ChromeOptions()
+            options.add_argument("headless")
+            options.add_argument("disable-gpu")
+
+            driver = webdriver.Chrome("C:/selenium/chromedriver", options=options)
+            url = "https://finance.daum.net/domestic/investors/KOSPI"  # 다음금융_코스피_탭
+
+            driver.get(url)
+
+            xpath1 = "//*[@id='boxDays']/div[2]/div/table/tbody/tr["
+            xpath2 = "]/td[3]"
+            xpath = xpath1 + str(k) + xpath2
+
+            kospiForeignBuying = WebDriverWait(driver, 12).until(EC.presence_of_element_located((By.XPATH, xpath))).text
+
+            # 받은 데이터를 int화 시킨다
+            kospiForeignBuying = int(kospiForeignBuying.replace(",", ""))
+
+            kospiForeignBuying_List.append(kospiForeignBuying)
+
+        # print("kospiForeignBuying_List: ", end="")  # (삭제) 확인용
+        # print(kospiForeignBuying_List)  # (삭제) 확인용
+
+        ## 일일 코스피 전체 외인 매매금액에 대한 top10 각 종목의 일일 매매금액의 비율을 구한다
+        ## 조건에 따른 메시지를 출력한다
+        # 아래의 z는 xx일 전 데이터를 의미한다
+        for z in range(0, 7):
+            # 각 종목의 일별 외인 매매금액(+,- 상관없음) / 코스피의 외인 매매금액 * 100 (반올림 2자리까지)
+            per_result = round(priceXamountList[z] / kospiForeignBuying_List[z] * 100, 2)
+            if z == 0:
+                # 종목별로 줄바꿈을 실시하기 위해 넣음
+                print("")
+            # 위 결과가 3% 넘을 경우 메시지 표기
+            if per_result >= 3:
+                # 각 종목별로 메시지 표기를 달리한다(인덱스 0~8까지 있음)
+                if i == 0:
+                    print("삼성전자: " + str(z+1) + "일 전에 외인들이 코스피의 " + str(per_result) + "% 만큼 매수함 / ", end="")
+                    # 3%를 넘긴하는데 코스피 & 개별 종목 둘 다 순매도일 때 출력
+                    if priceXamountList[z] <= 0 and kospiForeignBuying_List[z] <= 0:
+                        print("해당 일 코스피 & 개별종목 둘 다 순매도임")
+                elif i == 1:
+                    print("SK하이닉스:  " + str(z+1) + "일 전에 외인들이 코스피의 " + str(per_result) + "% 만큼 매수함 / ", end="")
+                    if priceXamountList[z] <= 0 and kospiForeignBuying_List[z] <= 0:
+                        print("해당 일 코스피 & 개별종목 둘 다 순매도임")
+                elif i == 2:
+                    print("LG화학:  " + str(z+1) + "일 전에 외인들이 코스피의 " + str(per_result) + "% 만큼 매수함 / ", end="")
+                    if priceXamountList[z] <= 0 and kospiForeignBuying_List[z] <= 0:
+                        print("해당 일 코스피 & 개별종목 둘 다 순매도임")
+                elif i == 3:
+                    print("삼성바이오:  " + str(z+1) + "일 전에 외인들이 코스피의 " + str(per_result) + "% 만큼 매수함 / ", end="")
+                    if priceXamountList[z] <= 0 and kospiForeignBuying_List[z] <= 0:
+                        print("해당 일 코스피 & 개별종목 둘 다 순매도임")
+                elif i == 4:
+                    print("셀트리온:  " + str(z+1) + "일 전에 외인들이 코스피의 " + str(per_result) + "% 만큼 매수함 / ", end="")
+                    if priceXamountList[z] <= 0 and kospiForeignBuying_List[z] <= 0:
+                        print("해당 일 코스피 & 개별종목 둘 다 순매도임")
+                elif i == 5:
+                    print("네이버:  " + str(z+1) + "일 전에 외인들이 코스피의 " + str(per_result) + "% 만큼 매수함 / ", end="")
+                    if priceXamountList[z] <= 0 and kospiForeignBuying_List[z] <= 0:
+                        print("해당 일 코스피 & 개별종목 둘 다 순매도임")
+                elif i == 6:
+                    print("삼성SDI:  " + str(z+1) + "일 전에 외인들이 코스피의 " + str(per_result) + "% 만큼 매수함 / ", end="")
+                    if priceXamountList[z] <= 0 and kospiForeignBuying_List[z] <= 0:
+                        print("해당 일 코스피 & 개별종목 둘 다 순매도임")
+                elif i == 7:
+                    print("현대차:  " + str(z+1) + "일 전에 외인들이 코스피의 " + str(per_result) + "% 만큼 매수함 / ", end="")
+                    if priceXamountList[z] <= 0 and kospiForeignBuying_List[z] <= 0:
+                        print("해당 일 코스피 & 개별종목 둘 다 순매도임")
+                elif i == 8:
+                    print("카카오:  " + str(z+1) + "일 전에 외인들이 코스피의 " + str(per_result) + "% 만큼 매수함 / ", end="")
+                    if priceXamountList[z] <= 0 and kospiForeignBuying_List[z] <= 0:
+                        print("해당 일 코스피 & 개별종목 둘 다 순매도임")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
